41b1a2a6f779ecced5a1d0b41ae43602d8867701
ngaionice <julian.myne@gmail.com>
2021-08-28 13:36:56 -0400

    Make minor changes for deployment to GH pages

--- src/Theme.js
+++ src/Theme.js
@@ -29,65 +29,71 @@ const theme = responsiveFontSizes(
 
 const drawerWidth = 320;
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-  },
-  paginator: {
-    justifyContent: "center",
-    padding: "10px",
-  },
-  searchBox: {
-    marginBottom: theme.spacing(1),
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    "& > *": {
-      marginTop: theme.spacing(1),
+const useStyles = makeStyles(
+  (theme) => ({
+    root: {
+      display: "flex",
     },
-  },
-  drawer: {
-    [theme.breakpoints.up("md")]: {
+    paginator: {
+      justifyContent: "center",
+      padding: "10px",
+    },
+    searchBox: {
+      marginBottom: theme.spacing(1),
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      "& > *": {
+        marginTop: theme.spacing(1),
+      },
+    },
+    drawer: {
+      [theme.breakpoints.up("md")]: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
+    },
+    appBar: {
+      [theme.breakpoints.up("md")]: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+      },
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up("md")]: {
+        display: "none",
+      },
+    },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    tabs: {
+      height: "48px",
+    },
+    drawerPaper: {
       width: drawerWidth,
-      flexShrink: 0,
     },
-  },
-  appBar: {
-    [theme.breakpoints.up("md")]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
     },
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up("md")]: {
-      display: "none",
+    loader: {
+      margin: theme.spacing(3),
     },
-  },
-  // necessary for content to be below app bar
-  toolbar: theme.mixins.toolbar,
-  tabs: {
-    height: "48px",
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-  loader: {
-    margin: theme.spacing(3),
-  },
-  expand: {
-    transform: "rotate(0deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: "rotate(180deg)",
-  },
-}));
+    expand: {
+      transform: "rotate(0deg)",
+      marginLeft: "auto",
+      transition: theme.transitions.create("transform", {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: "rotate(180deg)",
+    },
+  }),
+  { index: 1 }
+);
+
+// the {index: 1} is needed for production builds, as webpack messes with JSS precedence rules.
+// see https://stackoverflow.com/questions/62473898/material-ui-rendering-bugs-in-production-build
 
 export { theme, useStyles };
